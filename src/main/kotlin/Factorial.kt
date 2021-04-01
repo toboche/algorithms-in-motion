e9f50dd9d4ad01bc6197a1c576c3748cf753c0b1
Tomasz Bochenek <tomasz.bochenek@dazn.com>
2021-04-01 19:47:22 +0200

    factorial

--- src/main/kotlin/Factorial.kt
+++ src/main/kotlin/Factorial.kt
@@ -1,9 +1,3 @@
 class Factorial {
-    fun fact(x: Int): Int {
-        return if (x == 1) {
-            1
-        } else {
-            x * fact(x - 1)
-        }
-    }
+    fun fact(x: Int): Int = if (x == 1) 1 else x * fact(x - 1)
 }
